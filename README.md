2df37c8f6c6f7c411d92b0b1553a5119b73142e2
Krishna Kant Sharma <kksharma1618@gmail.com>
2017-07-19 13:42:19 +0530

    stash

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # lamda-restify
 A restify like interface for aws lamda with api gateway event
+
+# NOTICE
+In progress. Do not use.
--- src/lib/response.ts
+++ src/lib/response.ts
@@ -1,4 +1,5 @@
 import { LamdaCallback } from './lamda_callback'
+import Request from './request'
 import * as assert from 'assert-plus'
 import * as url from 'url'
 import { format as sprintf } from 'util'
@@ -24,8 +25,10 @@ const HEADER_ARRAY_BLACKLIST = {
 export default class Response {
     private _headers: { [key: string]: string | string[] } = {}
     private lamdaCallbackCalled = false
+    private statusCode: string
+    private _body: string
 
-    constructor(private lamdaCallback: LamdaCallback) {
+    constructor(private lamdaCallback: LamdaCallback, private req: Request) {
 
     }
     public cache(type?: any, options?: any) {
@@ -87,9 +90,9 @@ export default class Response {
     }
     public getHeaders() {
         let headers = {}
-        for(let name in this._headers) {
+        for (let name in this._headers) {
             let value = this._headers[name]
-            if(Array.isArray(value)) {
+            if (Array.isArray(value)) {
                 value = value.join(',')
             }
             headers[name] = value
@@ -111,11 +114,171 @@ export default class Response {
     }
     private __send() {
         this.callLamdaCallback()
+
+        let isHead = this.req.method === 'HEAD'
+        let code, body, headers, format
+
+        // derive arguments from types, one by one
+        let index = 0
+        // Check to see if the first argument is a status code
+        if (typeof arguments[index] === 'number') {
+            code = arguments[index++]
+        }
+
+        // Check to see if the next argument is a body
+        if (typeof arguments[index] === 'object' ||
+            typeof arguments[index] === 'string') {
+            body = arguments[index++]
+        }
+
+        // Check to see if the next argument is a collection of headers
+        if (typeof arguments[index] === 'object') {
+            headers = arguments[index++]
+        }
+
+        // Check to see if the next argument is the format boolean
+        if (typeof arguments[index] === 'boolean') {
+            format = arguments[index++]
+        }
+
+        // Now lets try to derive values for optional arguments that we were not
+        // provided, otherwise we choose sane defaults.
+
+        // If the body is an error object and we were not given a status code, try
+        // to derive it from the error object, otherwise default to 500
+        if (!code && body instanceof Error) {
+            code = (body as any).statusCode || 500
+        }
+
+        // Set sane defaults for optional arguments if they were not provided and
+        // we failed to derive their values
+        code = code || 200
+        headers = headers || {}
+
+        // Populate our response object with the derived arguments
+        this.statusCode = code
+        this._body = body
+        Object.keys(headers).forEach(k => {
+            this.header(k, headers[k])
+        })
+
+        
+        // Flush takes our constructed response object and sends it to the client
+        let _flush = (formattedBody) => {
+            this._data = formattedBody;
+
+            // Flush headers
+            this.writeHead(this.statusCode);
+
+            // Send body if it was provided
+            if (this._data) {
+                this.write(this._data);
+            }
+
+            // Finish request
+            this.end();
+
+            // If log level is set to trace, log the entire response object
+            if (log.trace()) {
+                log.trace({ res: this }, 'response sent');
+            }
+
+            // Return the response object back out to the caller of __send
+            return this;
+        }
+
+        // 204 = No Content and 304 = Not Modified, we don't want to send the
+        // body in these cases. HEAD never provides a body.
+        if (isHead || code === 204 || code === 304) {
+            return _flush();
+        }
+
+        // if no formatting, assert that the value to be written is a string
+        // or a buffer, then send it.
+        if (format === false) {
+            assert.ok(typeof body === 'string' || Buffer.isBuffer(body),
+                'res.sendRaw() accepts only strings or buffers');
+            return _flush(body);
+        }
+
+        // if no body, then no need to format. if this was an error caught by a
+        // domain, don't send the domain error either.
+        if (body === undefined || (body instanceof Error && body.domain)) {
+            return _flush();
+        }
+
+        // At this point we know we have a body that needs to be formatted, so lets
+        // derive the formatter based on the response object's properties
+
+        // _formatterError is used to handle any case where we were unable to
+        // properly format the provided body
+        function _formatterError(err) {
+            // If the user provided a non-success error code, we don't want to mess
+            // with it since their error is probably more important than our
+            // inability to format their message.
+            if (this.statusCode >= 200 && this.statusCode < 300) {
+                this.statusCode = err.statusCode;
+            }
+
+            log.warn({
+                req: this.req,
+                err: err
+            }, 'error retrieving formatter');
+
+            return _flush();
+        }
+
+        var formatter;
+        var type = this.contentType || this.getHeader('Content-Type');
+
+        // Check to see if we can find a valid formatter
+        if (!type && !this.req.accepts(this.acceptable)) {
+            return _formatterError(new errors.NotAcceptableError({
+                message: 'could not find suitable formatter'
+            }));
+        }
+
+        // Derive type if not provided by the user
+        if (!type) {
+            type = this.req.accepts(this.acceptable);
+        }
+
+        type = type.split(';')[0];
+
+        if (!this.formatters[type] && type.indexOf('/') === -1) {
+            type = mime.lookup(type);
+        }
+
+        // If we were unable to derive a valid type, default to treating it as
+        // arbitrary binary data per RFC 2046 Section 4.5.1
+        if (!this.formatters[type] && this.acceptable.indexOf(type) === -1) {
+            type = 'application/octet-stream';
+        }
+
+        formatter = this.formatters[type] || this.formatters['*/*'];
+
+        // If after the above attempts we were still unable to derive a formatter,
+        // provide a meaningful error message
+        if (!formatter) {
+            return _formatterError(new errors.InternalServerError({
+                message: 'could not find formatter for application/octet-stream'
+            }));
+        }
+
+        if (this._charSet) {
+            type = type + '; charset=' + this._charSet;
+        }
+
+        // Update header to the derived content type for our formatter
+        this.setHeader('Content-Type', type);
+
+        // Finally, invoke the formatter and flush the request with it's results
+        return _flush(formatter(this.req, this, body));
     }
     private callLamdaCallback() {
         if (!this.lamdaCallbackCalled) {
             this.lamdaCallbackCalled = true
-            
+
             this.lamdaCallback(null, {
                 statusCode: '200',
                 body: '',
