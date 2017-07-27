d6a86884791211d7c95d3ab40459f80996020f08
Krishna Kant Sharma <kksharma1618@gmail.com>
2017-07-27 13:48:15 +0530

    bump package. expose restify-errors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ export {default as Response} from './lib/response'
 export {ServerOptions, FormatterFunction, Formatters} from './lib/server_options'
 export {EventSource} from './lib/event_source'
 export {LamdaCallback} from './lib/lambda_callback'
+import * as restifyErrors from 'restify-errors'
+export const errors = restifyErrors
 
 import Server from './lib/server'
 export default Server
